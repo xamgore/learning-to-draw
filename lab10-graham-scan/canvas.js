7dacc875bb69bac35a15645429fed66aa05d43f5
xamgore <xamgore@ya.ru>
2016-11-05 03:29:50 +0300

    Implement graham scan

---
+++ lab10-graham-scan/canvas.js
@@ -0,0 +1,23 @@
+let $       = (s) => document.querySelector(s),
+    $$      = (s) => document.querySelectorAll(s),
+    $canvas = $('canvas'),
+    ctx     = $canvas.getContext("2d"),
+    width   = 800,
+    height  = 600,
+    drawing = false;
+
+ctx.canvas.width = width;
+ctx.canvas.height = height;
+
+NodeList.prototype.forEach = Array.prototype.forEach;
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+
+Array.prototype.equals = function([x, y, _]) {
+    return this[0] == x && this[1] == y;
+}
+
+Array.prototype.remove = function(elem) {
+    let copy = this.slice(0, this.length);
+    copy.splice(this.indexOf(elem), 1);
+    return copy;
+};
